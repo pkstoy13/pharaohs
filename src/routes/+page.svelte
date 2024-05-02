a3c787c91290514de78e094d466b2f4a8cb791f4
pkstoy13 <pkstoyanov1@gmail.com>
2024-05-02 13:38:48 -0400

    added some gods

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,4 +4,17 @@
 	<li>Narmer</li>
 	<li>Hor Aha</li>
 	<li>Menes?</li>
+	<li>King Den</li>
 </ul>
+<h1>Gods:</h1>
+<ul>
+	<li>Horus</li>
+	<li>Amun</li>
+	<li>Neith & Neithotep the Queen</li>
+	<li>Anuket, Goddess of the Cataracts</li>
+	<li>Seshat, Goddess of measurement and writing</li>
+	<li>Thoth, God of wisdom and writing</li>
+</ul>
+<div>
+	<div>Karnak - Temple of Amun</div>
+</div>
