e45b133bb66ba614b336a8053a0e7706f7f5e45d
pkstoy13 <pkstoyanov1@gmail.com>
2024-05-05 14:22:01 -0400

    additional changes, starting to understand

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,5 @@
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<h2 class="font-bold">KINGS:</h2>
-<div class="flex underline ml-4">
+<h1>Kings:</h1>
+<div class="flex ml-4 my-2">
 	<ul>
 		<li>Narmer</li>
 		<li>Hor Aha</li>
@@ -9,20 +8,45 @@
 	</ul>
 </div>
 <h1>Gods:</h1>
-<ul>
-	<li>Horus</li>
-	<li>Amun</li>
-	<li>Neith & Neithotep the Queen</li>
-	<li>Anuket, Goddess of the Cataracts</li>
-	<li>Seshat, Goddess of measurement and writing</li>
-	<li>Thoth, God of wisdom and writing</li>
-</ul>
-<div>
-	<div>Karnak - Temple of Amun</div>
+<div class="flex ml-4 my-2">
+	<ul>
+		<li>Horus</li>
+		<li>Amun</li>
+		<li>Neith & Neithotep the Queen</li>
+		<li>Anuket, Goddess of the Cataracts</li>
+		<li>Seshat, Goddess of measurement and writing</li>
+		<li>Thoth, God of wisdom and writing</li>
+		<li>Anubis, God of the Dead and Embalming</li>
+		<li>Re the Sun God</li>
+	</ul>
+</div>
+<h1>Locations:</h1>
+<div class="flex ml-4 my-2">
+	<ul>
+		<li>Karnak - Temple of Amun</li>
+		<li>Mount Anubis</li>
+		<li>Memphis, Old Capital</li>
+		<li>Duat (Land of the Dead)</li>
+	</ul>
+</div>
+<h1>Artifacts:</h1>
+<div class="flex ml-4 my-2">
+	<ul>
+		<li>Wedjat (Eye of Horus)</li>
+	</ul>
 </div>
 
 <style lang="postcss">
 	:global(html) {
 		background-color: theme(colors.indigo.400);
+	}
+	li {
+		background-color: theme(colors.amber.100);
+	}
+	h1 {
+		font-size: 2em;
+		font-weight: bold;
+		border-bottom: 4px black solid;
+		background-color: fuchsia;
 	}
 </style>
